6943fe0cae7bc5be63e2df464d68b96871cd552e
CadeL4D <32691889+CadeL4D@users.noreply.github.com>
2025-06-18 23:59:15 -0500

    Collision Through Math

--- Scripts/drawing_space.gd
+++ Scripts/drawing_space.gd
@@ -3,6 +3,7 @@ extends Control
 var clicked = false
 var event_pos = null
 var lineList = {}
+var redoList = []
 var UUID = 0
 var newLine = Line2D.new()
 var line
@@ -12,13 +13,13 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("click"):
-		#lineList[UUID].append(lineList[lineList.keys()[-1]][0].get_points())
-		print(lineList)
 		addingNewLine(1)
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("undo"):
 		undo()
+	if Input.is_action_just_pressed("redo"):
+		redo()
 	if not Input.is_action_pressed("click"):
 		return
 	event_pos = event.position
@@ -27,11 +28,13 @@ func _input(event: InputEvent) -> void:
 func _draw() -> void:
 	if(event_pos != null) and Input.is_action_pressed("click"):
 		lineList[UUID][0].add_point(event_pos)
+		lineList[UUID][1].append(event_pos)
+		
 
 		
 func addingNewLine(flip = 0):
 		UUID += flip
-		lineList[UUID] = [Line2D.new()]
+		lineList[UUID] = [Line2D.new(),[]]
 		lineList[UUID][0].default_color = $tool_selection/HBoxContainer/ColorPickerButton.color
 		lineList[UUID][0].antialiased = true
 		lineList[UUID][0].begin_cap_mode = 2
@@ -40,10 +43,22 @@ func addingNewLine(flip = 0):
 		lineList[UUID][0].end_cap_mode = 2
 		lineList[UUID][0].round_precision = 10
 		self.add_child(lineList[UUID][0])
+		print(UUID)
 		
 func undo():
+	redoList.append(lineList[lineList.keys()[-2]][1])
 	lineList[lineList.keys()[-2]][0].clear_points()
 	lineList.erase(lineList.keys()[-2])
+
+func redo():
+	if redoList.size() > 0:
+		for coords in redoList[-1]:
+			lineList[UUID][0].add_point(coords)
+		redoList.remove_at(redoList.size()-1)
+		addingNewLine(1)
+	if redoList.size() < 20:
+		redoList.remove_at(0)
+	
 
 
 func _on_tool_selection_pressed_button(extra_arg_0) -> void:
--- Scripts/testing_scene.gd
+++ Scripts/testing_scene.gd
@@ -1,5 +1,14 @@
 extends Control
 
 
-func _on_file_dialog_dir_selected(dir: String) -> void:
-	print(dir)
+func _ready() -> void:
+	print($Line2D.points)
+	print(float(213-120)/float(513-240))
+	print(get_global_mouse_position().y)
+	print(0.34065934065934*get_global_mouse_position().x)
+func _process(delta: float) -> void:
+	if get_global_mouse_position().y == 0.34065934065934*get_global_mouse_position().x:
+		print("Wtf")
+	#if get_global_mouse_position().x > 240 and get_global_mouse_position().x < 513:
+		#if get_global_mouse_position().y > 120 and get_global_mouse_position().y < 213:
+			#print(get_global_mouse_position())
