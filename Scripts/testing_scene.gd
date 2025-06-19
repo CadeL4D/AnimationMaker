29399dac3bf91075cb064a50fec6116ba464c927
CadeL4D <32691889+CadeL4D@users.noreply.github.com>
2025-06-18 21:58:33 -0500

    Push
    AddedFuckingSaving

---
+++ Scripts/testing_scene.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_file_dialog_dir_selected(dir: String) -> void:
+	print(dir)
--- export_buttons.gd
+++ export_buttons.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var AddButton = $VBoxContainer/Add
 @onready var PlayButton = $VBoxContainer/Watch
 @onready var SaveButton = $VBoxContainer/Save
+@onready var FileSave = $dirSave
 
 func _ready() -> void:
 	AddButton.pressed.connect(add_button_clicked)
@@ -19,6 +20,26 @@ func play_button_clicked():
 	get_images("user://")
 			
 func save_button_clicked():
+	FileSave.show()
+
+func num_files_in(path):
+	var files = []
+	var dir = DirAccess.open(path)
+	if dir:
+		return len((dir.get_files()))
+
+func get_images(path):
+	var images = []
+	var dir = DirAccess.open(path)
+	if dir:
+		print(dir.get_files())
+		for i in dir.get_files():
+			var image = Image.load_from_file(path+str(i))
+			images.append(image)
+	return images
+			
+
+func _on_dir_save_file_selected(path: String) -> void:
 	const GIFExporter = preload("res://gdgifexporter/exporter.gd")
 	# load quantization module that you want to use
 	const MedianCutQuantization = preload("res://gdgifexporter/quantization/median_cut.gd")
@@ -37,26 +58,9 @@ func save_button_clicked():
 
 	# when you have exported all frames of animation you, then you can save data into file
 	# open new file with write privlige
-	var file: FileAccess = FileAccess.open('user://result.gif', FileAccess.WRITE)
+	
+	var file: FileAccess = FileAccess.open(path, FileAccess.WRITE)
 	# save data stream into file
 	file.store_buffer(exporter.export_file_data())
 	# close the file
 	file.close()
-
-func num_files_in(path):
-	var files = []
-	var dir = DirAccess.open(path)
-	if dir:
-		return len((dir.get_files()))
-
-func get_images(path):
-	var images = []
-	var dir = DirAccess.open(path)
-	if dir:
-		print(dir.get_files())
-		for i in dir.get_files():
-			var image = Image.load_from_file(path+str(i))
-			images.append(image)
-	return images
-			
-			
