ad76aeb39747be22b85586a8e610daa3e1de536b
wardmax <96925365+wardmax@users.noreply.github.com>
2025-07-18 21:32:44 -0500

    d

--- Scripts/drawing_space.gd
+++ Scripts/drawing_space.gd
@@ -8,7 +8,8 @@ var UUID = 0
 var newLine = Line2D.new()
 var line
 var selected_tool = "paint"
-@export var tool_radius = 100
+var tool_radius = 100
+var background_color = Color.WHITE
 
 func _ready() -> void:
 	addingNewLine()
@@ -39,22 +40,27 @@ func _draw() -> void:
 		lineList[UUID][0].add_point(event_pos)
 		lineList[UUID][1].append(event_pos)
 	elif(event_pos != null) and Input.is_action_pressed("click") and selected_tool == "eraser":
-		erase_points()
+		
+		lineList[UUID][0].width = tool_radius
+		lineList[UUID][0].add_point(event_pos)
+		lineList[UUID][1].append(event_pos)
 
 func addingNewLine(flip = 0):
 	#test if tool is paint before adding new Line2D otherwise it creates one with other tools
-	if(selected_tool == "paint"):
-		UUID += flip
-		lineList[UUID] = [Line2D.new(),[]]
+	UUID += flip
+	lineList[UUID] = [Line2D.new(),[]]
+	if selected_tool == "eraser":
+		lineList[UUID][0].default_color = background_color
+	else:
 		lineList[UUID][0].default_color = $tool_selection/HBoxContainer/ColorPickerButton.color
-		lineList[UUID][0].antialiased = true
-		lineList[UUID][0].begin_cap_mode = 2
-		lineList[UUID][0].joint_mode = 2
-		lineList[UUID][0].width = 10
-		lineList[UUID][0].end_cap_mode = 2
-		lineList[UUID][0].round_precision = 10
-		$ColorRect.add_child(lineList[UUID][0])
-		print(UUID)
+	lineList[UUID][0].antialiased = true
+	lineList[UUID][0].begin_cap_mode = 2
+	lineList[UUID][0].joint_mode = 2
+	lineList[UUID][0].width = 10
+	lineList[UUID][0].end_cap_mode = 2
+	lineList[UUID][0].round_precision = 10
+	$ColorRect.add_child(lineList[UUID][0])
+	print(UUID)
 		
 func undo():
 	redoList.append(lineList[lineList.keys()[-2]][1])
@@ -70,14 +76,15 @@ func redo():
 	if redoList.size() < 20:
 		redoList.remove_at(0)
 	
-func erase_points():
+#func erase_points():
+	#
 	#iterate through all lineNums created and test to see if coordinates are within a radius.
-		for lineNum in range(UUID):
-			var index = 0 #keep track of index as required by remove_point method
-			for point in (lineList[lineNum][0].points):
-				if(event_pos.distance_to(point) < tool_radius):
-					lineList[lineNum][0].remove_point(index)
-				index += 1
+		#for lineNum in range(UUID):
+			#var index = 0 #keep track of index as required by remove_point method
+			#for point in (lineList[lineNum][0].points):
+				#if(event_pos.distance_to(point) < tool_radius):
+					#lineList[lineNum][0].remove_point(index)
+				#index += 1
 	
 func _on_tool_selection_pressed_button(extra_arg_0) -> void:
 	if(extra_arg_0 == "paint"):
